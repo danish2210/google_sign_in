7b018ad5f6af4998de3ad0e3f9512ba622fef286
Danish Anwar <da.ruvyx@gmail.com>
2022-01-24 00:24:55 +0500

    redirect protector now checks for subdomains

--- lib/google_sign_in/redirect_protector.rb
+++ lib/google_sign_in/redirect_protector.rb
@@ -9,7 +9,7 @@ module GoogleSignIn
     QUALIFIED_URL_PATTERN = /\A#{URI::DEFAULT_PARSER.make_regexp}\z/
 
     def ensure_same_origin(target, source)
-      if target.blank? || (target =~ QUALIFIED_URL_PATTERN && origin_of(target) != origin_of(source))
+      if target.blank? || (target =~ QUALIFIED_URL_PATTERN && origin_of(target).include?(origin_of(source)))
         raise Violation, "Redirect target #{target.inspect} does not have same origin as request (expected #{origin_of(source)})"
       end
     end
@@ -17,7 +17,7 @@ module GoogleSignIn
     private
       def origin_of(url)
         uri = URI(url)
-        "#{uri.scheme}://#{uri.host}:#{uri.port}"
+        uri.host
       rescue ArgumentError
         nil
       end
