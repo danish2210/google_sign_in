c814859fe3a4075d38cc40b72e422f399de276ff
Danish Anwar <da.ruvyx@gmail.com>
2022-01-24 04:52:19 +0500

    moved proceed_to to session var instead of flash

--- app/controllers/google_sign_in/authorizations_controller.rb
+++ app/controllers/google_sign_in/authorizations_controller.rb
@@ -4,16 +4,19 @@ class GoogleSignIn::AuthorizationsController < GoogleSignIn::BaseController
   skip_forgery_protection only: :create
 
   def create
+    session[:oauth_proceed_to] = params.require(:proceed_to)
+    session[:oauth_state] = state
     redirect_to login_url(scope: 'openid profile email', state: state),
-      allow_other_host: true, flash: { proceed_to: params.require(:proceed_to), state: state }
+                allow_other_host: true, flash: { proceed_to: params.require(:proceed_to), state: state }
   end
 
   private
-    def login_url(**params)
-      client.auth_code.authorize_url(prompt: 'login', **params)
-    end
 
-    def state
-      @state ||= SecureRandom.base64(24)
-    end
+  def login_url(**params)
+    client.auth_code.authorize_url(prompt: 'login', **params)
+  end
+
+  def state
+    @state ||= SecureRandom.base64(24)
+  end
 end
--- app/controllers/google_sign_in/callbacks_controller.rb
+++ app/controllers/google_sign_in/callbacks_controller.rb
@@ -3,35 +3,36 @@ require 'google_sign_in/redirect_protector'
 class GoogleSignIn::CallbacksController < GoogleSignIn::BaseController
   def show
     redirect_to proceed_to_url, flash: { google_sign_in: google_sign_in_response }
-  rescue GoogleSignIn::RedirectProtector::Violation => error
-    logger.error error.message
+  rescue GoogleSignIn::RedirectProtector::Violation => e
+    logger.error e.message
     head :bad_request
   end
 
   private
-    def proceed_to_url
-      flash[:proceed_to].tap { |url| GoogleSignIn::RedirectProtector.ensure_same_origin(url, request.url) }
-    end
 
-    def google_sign_in_response
-      if valid_request? && params[:code].present?
-        { id_token: id_token }
-      else
-        { error: error_message_for(params[:error]) }
-      end
-    rescue OAuth2::Error => error
-      { error: error_message_for(error.code) }
-    end
+  def proceed_to_url
+    session[:oauth_proceed_to].tap { |url| GoogleSignIn::RedirectProtector.ensure_same_origin(url, request.url) }
+  end
 
-    def valid_request?
-      flash[:state].present? && params[:state] == flash[:state]
+  def google_sign_in_response
+    if valid_request? && params[:code].present?
+      { id_token: id_token }
+    else
+      { error: error_message_for(params[:error]) }
     end
+  rescue OAuth2::Error => e
+    { error: error_message_for(e.code) }
+  end
 
-    def id_token
-      client.auth_code.get_token(params[:code])['id_token']
-    end
+  def valid_request?
+    session[:oauth_state].present? && params[:state] == session[:oauth_state]
+  end
 
-    def error_message_for(error_code)
-      error_code.presence_in(GoogleSignIn::OAUTH2_ERRORS) || "invalid_request"
-    end
+  def id_token
+    client.auth_code.get_token(params[:code])['id_token']
+  end
+
+  def error_message_for(error_code)
+    error_code.presence_in(GoogleSignIn::OAUTH2_ERRORS) || 'invalid_request'
+  end
 end
